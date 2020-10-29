9c437117ac957b44dca264daa34de25df127b670
balez4464 <balez4464@wrdsb.ca>
2020-10-29 15:26:14 +0000

    created a program that says hello to the user using a j label

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JTextField inputBox;
+
+  JLabel usersName;
+
+  JButton sayHello; 
+
+  JPanel mainPanel;
 
 
   // Method to assemble our GUI
@@ -18,6 +24,38 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+    //intialize main panel and show it
+    mainPanel = new JPanel();
+    mainPanel.setLayout(null);
+    frame.add(mainPanel);
+
+    //sets the text field to type in usersName
+    inputBox = new JTextField();
+    inputBox.setBounds(150, 50, 500, 25);
+
+    //shows text inputBox
+    mainPanel.add(inputBox);
+
+    //creates j label to show usersName
+    usersName = new JLabel("");
+    usersName.setBounds(150, 95, 200, 20);
+
+    //shows users usersName
+    mainPanel.add(usersName);
+
+    //makes button for user to click
+    sayHello = new JButton("Say Hello");
+    sayHello.setBounds(250, 145, 300, 50);
+
+    //sets action command for say Hello
+    sayHello.setActionCommand("hello");
+
+    //adds action listener for button
+    sayHello.addActionListener(this);
+
+    //adds button to the main panel
+    mainPanel.add(sayHello);
  
     
 
@@ -27,6 +65,17 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+
+    //if button is pressed
+    if(command.equals("hello")){
+      //reprints the j label to hello plus users name
+      String helloUser = inputBox.getText();
+      usersName.setText("Hello " + helloUser);
+      //if there is no text in the box
+    }else{
+      usersName.setText("");
+    }
+    
 
   }
 
